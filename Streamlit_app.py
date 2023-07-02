a438cbf7792f31cec01ba6232b8cd8126719d6cb
Movva918 <138343573+Movva918@users.noreply.github.com>
2023-07-02 16:14:58 -0400

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -4,5 +4,5 @@ streamlit.title('My parents New healty Meal Plan')
 
 streamlit.header('BreakFast Menu')
 streamlit.text('Omega3 & Bluberry oatMeal')
-streamlit.text(Kale, Spinach & Roceket Smoothie)
+streamlit.text('Kale, Spinach & Roceket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
