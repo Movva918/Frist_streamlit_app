22a1d6399c903f39e155c4c964bfca69e5412d07
Movva918 <138343573+Movva918@users.noreply.github.com>
2023-07-02 16:14:16 -0400

    Update Streamlit_app.py
    Added Breakfast Menu

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My parents New healty Meal Plan')
+
+streamlit.header('BreakFast Menu')
+streamlit.text('Omega3 & Bluberry oatMeal')
+streamlit.text(Kale, Spinach & Roceket Smoothie)
+streamlit.text('Hard-Boiled Free-Range Egg')
