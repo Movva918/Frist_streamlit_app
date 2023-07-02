bcd672386d115f86bd45f84576f497e908406cf4
Movva918 <138343573+Movva918@users.noreply.github.com>
2023-07-02 16:25:30 -0400

    Update Streamlit_app.py
    Added emojis

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -2,7 +2,10 @@ import streamlit
 
 streamlit.title('My parents New healty Meal Plan')
 
-streamlit.header('BreakFast Menu')
-streamlit.text('Omega3 & Bluberry oatMeal')
-streamlit.text('Kale, Spinach & Roceket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('BreakFast Favourites')
+streamlit.text('🥣 Omega3 & Bluberry oatMeal')
+streamlit.text('🥗 Kale, Spinach & Roceket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avacado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
