a3a1beb8b0fb783f6a0f4027b698e65c614f5d9b
Movva918 <138343573+Movva918@users.noreply.github.com>
2023-07-04 13:06:17 -0400

    Update Streamlit_app.py
    added function

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -25,6 +25,11 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+# create the repeatable code block ( called a function)
+def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
 # New Section to display Fruityice api response
 streamlit.header('Fruityvice Fruit Advice!')
 try:
@@ -32,9 +37,8 @@ try:
   if not  fruit_choice:
       streamlit.error("Please select a fruit to get information.")
   else:
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-      streamlit.dataframe(fruityvice_normalized)
+       back_from_function = get_fruityvice_data(fruit_choice)
+      streamlit.dataframe(back_from_function)
 except URLError as e:
    streamlit.error()
 streamlit.write('The user entered ', fruit_choice)
@@ -42,6 +46,7 @@ streamlit.write('The user entered ', fruit_choice)
 
 
 # change jason version to normalized form
+
 
 # shows the data in table form
 
